c0349b020a0ae4d5e11ae38aafaa0c7e30ca85b7
lidianowak <lidia.nowak@doctoral.uj.edu.pl>
2025-05-31 21:13:33 +0200

    Dodano ścieżkę szablonów HTML do settings.py

--- katalog/settings.py
+++ katalog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'katalog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'biblioteka/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
